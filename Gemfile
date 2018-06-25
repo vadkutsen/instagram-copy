f434ec95cf421410d808ddd36af33dc02e5dd239
vadkutsen <vadkutsen@gmail.com>
2018-06-26 00:51:43 +0200

    Add devise user

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'haml', '~> 5.0', '>= 5.0.4'
 gem 'simple_form', '~> 4.0', '>= 4.0.1'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'devise', '~> 4.4', '>= 4.4.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,11 +1,11 @@
 class PicsController < ApplicationController
-	before_action :find_pic, only: [:show, :update, :destroy]
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@pics = Pic.all.order("created_at DESC")
 	end
 
 	def show
-
 	end
 
 
@@ -20,6 +20,22 @@ class PicsController < ApplicationController
 		else
 			render 'new'
 		end 
+	end
+
+	def edit
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic was updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
 	end
 
 	private
